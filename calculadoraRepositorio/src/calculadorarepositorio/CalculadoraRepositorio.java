49ba72c97443b81b27bd8f5d69bd71717a075f54
gabrielaGIT <gabriela@ejemplo.com>
2025-04-30 20:55:13 +0200

    Versión inicial con método sumar

---
+++ calculadoraRepositorio/src/calculadorarepositorio/CalculadoraRepositorio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadorarepositorio;
+
+/**
+ *
+ * @author Gabriela Esteban
+ */
+public class CalculadoraRepositorio {
+
+    public int sumar(int a, int b) {
+        return a + b;
+    }    
+}
